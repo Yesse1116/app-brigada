edf0c95ab4b206c91a6c3aced1384ad88707099d
lmiranda20 <lucianogabrielmiranda2718@gmail.com>
2024-12-02 23:56:18 -0300

    cerrar sesion

--- front/src/componentes/Navbar/Navbar.jsx
+++ front/src/componentes/Navbar/Navbar.jsx
@@ -1,20 +1,26 @@
-import './navbar.css'
-import { Link } from "react-router-dom";
-import {useAuth} from '../Auth/authContext'
-function Navbar (){
-    const {logout} = useAuth;
-    const handleLogout= ()=>{
-      logout()
-      alert ('Sesion cerrada')
-    }
-    return(
+import './navbar.css'; 
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from '../Auth/authContext';
+
+function Navbar() {
+    const { logout } = useAuth();
+    const navigate = useNavigate(); 
+
+    const handleLogout = () => {
+        logout();
+        alert('Sesión cerrada');
+        navigate('/auth/login');
+    };
+
+    return (
         <nav className='nav'>
             <ul>
-                <li><Link to= "/sesion"> Inicio</Link></li>
-                <li><Link to= "/calendario">Fechas Importantes</Link></li>
-                <li><Link onClick={handleLogout}> Cerrar Sesion</Link></li>
+                <li><Link to="/sesion">Inicio</Link></li>
+                <li><Link to="/calendario">Fechas Importantes</Link></li>
+                <li><Link to="/auth/login" onClick={handleLogout}> Cerrar Sesion</Link></li>
             </ul>
         </nav>
-    )
+    );
 }
-export default Navbar
+
+export default Navbar;
